a0b3cd2ffe58695c2c5f435e06beeb9e7febc7db
Paul guadalupe veliz lopez <paulsystem32@gmail.com>
2021-02-06 16:05:52 -0700

    Integracion de EntityFramework

--- src/dulce-aroma/LoginForm.cs
+++ src/dulce-aroma/LoginForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace dulce_aroma
 {
-    public partial class Form1 : Form
+    public partial class LoginForm : Form
     {
-        public Form1()
+        public LoginForm()
         {
             InitializeComponent();
         }
