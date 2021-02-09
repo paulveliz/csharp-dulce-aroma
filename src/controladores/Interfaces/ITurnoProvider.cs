efe3faca2ce4856e1c7eb9fc9cec9d83169c3e12
Paul guadalupe veliz lopez <paulsystem32@gmail.com>
2021-02-08 21:20:53 -0700

    TurnoController & Provider
    Se elaboro la interface provider para turnos y tambien su controlador.

--- src/controladores/Interfaces/ITurnoProvider.cs
+++ src/controladores/Interfaces/ITurnoProvider.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using modelos.Context;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace controladores.Interfaces
 {
     interface ITurnoProvider
     {
-
+        Task<Turnos> ObtenerPorId(int idTurno);
+        Task<IEnumerable<Turnos>> TurnosPorFecha(DateTime fromt, DateTime to);
+        Task<IEnumerable<Turnos>> TurnosPorEmpleado(DateTime fromt, DateTime to, int idEmpleado);
+        Task<(bool isSucess, Turnos turno)> CrearNuevo(Turnos turno);
+        Task<(bool isSucess, Turnos turno)> ModificarEstatus(int idTurno, int nuevoEstatus);
     }
 }
--- src/controladores/TurnoController.cs
+++ src/controladores/TurnoController.cs
@@ -1,12 +1,81 @@
-﻿using System;
+﻿using controladores.Interfaces;
+using modelos.Context;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace controladores
 {
-    class TurnoController
+    public class TurnoController : ITurnoProvider
     {
+        public async Task<(bool isSucess, Turnos turno)> CrearNuevo(Turnos turno)
+        {
+            using (var db = new dulce_aroma_db())
+            {
+                var nuevoTurno = db.Turnos.Add(turno);
+                var aff = await db.SaveChangesAsync();
+                return aff > 0 ? (true, nuevoTurno) : (false, nuevoTurno);
+            }
+        }
+
+        public async Task<(bool isSucess, Turnos turno)> ModificarEstatus(int idTurno, int nuevoEstatus)
+        {
+            using (var db = new dulce_aroma_db())
+            {
+                var t = await db.Turnos.FirstOrDefaultAsync(tr => tr.idEstatus == idTurno);
+                t.idEstatus = nuevoEstatus;
+                var aff = await db.SaveChangesAsync();
+                return aff > 0 ? (true, t) : (false, t);
+            }
+        }
+
+        public async Task<Turnos> ObtenerPorId(int idTurno)
+        {
+            using (var db = new dulce_aroma_db())
+            {
+                var t = await db.Turnos.FirstOrDefaultAsync(tr => tr.idEstatus == idTurno);
+                return t;
+            }
+        }
+
+        public async Task<IEnumerable<Turnos>> TurnosPorEmpleado(DateTime fromt, DateTime to, int idEmpleado)
+        {
+            using (var db = new dulce_aroma_db())
+            {
+                var turnos = await db.Turnos
+                                    .Include(t => t.cTurnoEstatus)
+                                    .Include(t => t.Empleados)
+                                    .Include(t => t.Ventas)
+                                    .Where(t =>
+                                        t.fecha >= fromt &&
+                                        t.fecha <= to &&
+                                        t.idEmpleado == idEmpleado
+                                    )
+                                    .OrderByDescending(t => t.id)
+                                    .ToListAsync();
+                return turnos;
+            }
+        }
+
+        public async Task<IEnumerable<Turnos>> TurnosPorFecha(DateTime fromt, DateTime to)
+        {
+            using (var db = new dulce_aroma_db())
+            {
+                var turnos = await db.Turnos
+                                    .Include(t => t.cTurnoEstatus)
+                                    .Include(t => t.Empleados)
+                                    .Include(t => t.Ventas)
+                                    .Where(t =>
+                                        t.fecha >= fromt &&
+                                        t.fecha <= to 
+                                    )
+                                    .OrderByDescending(t => t.id)
+                                    .ToListAsync();
+                return turnos;
+            }
+        }
     }
 }
